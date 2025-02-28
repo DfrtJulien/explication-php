1f2347c82e79ce3a1d2f6f4e0384623cace78fa6
DfrtJulien <de.freitas.julien78@gmail.com>
2025-02-28 22:49:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require "vendor/autoload.php";
+
+
+
+
+use Config\Router;
+
+$router = new Router();
+
+//la page d'accueil
+$router->addRoute('/', 'HomeController', 'index');
+
+
+$router->handleRequest();
